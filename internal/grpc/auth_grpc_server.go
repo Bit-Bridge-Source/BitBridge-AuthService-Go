46201ea96e1dcce7ef8f8a387f6fac2257e5cc34
JonathanDowdell <jdowde2@wgu.edu>
2023-10-22 17:12:54 -0700

    Added Comments to gRRPC Server

--- internal/grpc/auth_grpc_server.go
+++ internal/grpc/auth_grpc_server.go
@@ -10,6 +10,7 @@ import (
 	"google.golang.org/grpc"
 )
 
+// IAuthGRPCServer is an interface defining the authentication related methods that the GRPC server should implement.
 type IAuthGRPCServer interface {
 	Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error)
 	Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error)
@@ -17,23 +18,26 @@ type IAuthGRPCServer interface {
 	InitServer(port string, listener Listener) error
 }
 
+// ServerConfig holds configuration for the server such as Listener and GRPCServer.
 type ServerConfig struct {
 	Listener   net.Listener
 	GRPCServer *grpc.Server
 }
 
+// Listener interface defines the Listen method for network connections.
 type Listener interface {
 	Listen(network, address string) (net.Listener, error)
 }
 
+// AuthGRPCServer is a struct that embeds the services and configurations needed for the authentication server.
 type AuthGRPCServer struct {
-	AuthService service.IAuthService
-	Middleware  grpc.UnaryServerInterceptor
-	Config      ServerConfig
-	pb.UnimplementedAuthServiceServer
+	AuthService                       service.IAuthService        // Authentication service
+	Middleware                        grpc.UnaryServerInterceptor // Middleware for unary server interactions
+	Config                            ServerConfig                // Server configuration
+	pb.UnimplementedAuthServiceServer                             // Embedding the unimplemented server for forward compatibility
 }
 
-// Initialize server but do not serve yet
+// InitServer initializes the server with the given port and listener but doesn’t start it.
 func (s *AuthGRPCServer) InitServer(port string, listener Listener) error {
 	lis, err := listener.Listen("tcp", port)
 	if err != nil {
@@ -47,11 +51,12 @@ func (s *AuthGRPCServer) InitServer(port string, listener Listener) error {
 	return nil
 }
 
-// Start serving
+// Run starts the GRPC server, and it will run until it's stopped.
 func (s *AuthGRPCServer) Run() error {
 	return s.Config.GRPCServer.Serve(s.Config.Listener)
 }
 
+// Login handles the login requests, authenticating users and returning tokens.
 func (s *AuthGRPCServer) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
 	loginModel := &public_model.LoginModel{
 		Email:    req.GetEmail(),
@@ -69,6 +74,7 @@ func (s *AuthGRPCServer) Login(ctx context.Context, req *pb.LoginRequest) (*pb.L
 	}, nil
 }
 
+// Register handles user registration requests, creating user accounts and returning tokens.
 func (s *AuthGRPCServer) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
 	registerModel := &public_model.RegisterModel{
 		Email:    req.GetEmail(),
@@ -87,5 +93,5 @@ func (s *AuthGRPCServer) Register(ctx context.Context, req *pb.RegisterRequest) 
 	}, nil
 }
 
-// Ensure AuthGRPCServer implements IAuthGRPCServer.
+// Ensuring at compile time that AuthGRPCServer implements IAuthGRPCServer interface.
 var _ IAuthGRPCServer = (*AuthGRPCServer)(nil)
